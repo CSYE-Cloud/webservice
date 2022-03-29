572fdfbfe678a10f5268bf10706602f5988d9389
Garima Choudhary <90646523+GarimaSChoudhary@users.noreply.github.com>
2022-03-28 21:21:03 -0400

    Update AppConfig.java

--- src/main/java/com/cloud/application/config/AppConfig.java
+++ src/main/java/com/cloud/application/config/AppConfig.java
@@ -18,7 +18,7 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 public class AppConfig {
 	    
 	    @Bean
-		public PasswordEncoder encoder() {
+		public BCryptPasswordEncoder encoder() {
 			return new BCryptPasswordEncoder();
 		}
 
