c794d9b6740e546199f0be76b2e2e9d4de8d82c4
GarimaSChoudhary <choudhary.g@northeastern.edu>
2022-04-05 21:36:16 -0400

    assignment 8

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -7,7 +7,14 @@ echo "in after install"
 #sudo kill -9 `sudo lsof -t -i:8080`
 pid=$(sudo lsof -i tcp:8080 -t)
 echo $pid
-
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/home/ec2-user/cloudwatch-config.json \
+    -s
+sudo /bin/systemctl status amazon-cloudwatch-agent
+sudo /bin/systemctl stop amazon-cloudwatch-agent
+sudo /bin/systemctl start amazon-cloudwatch-agent
 # sudo systemctl start webapp.service
 # sudo chown ec2-user:ec2-user
 #removing previous build ROOT folder
