83b850d2efd5b40fd9c32f948ef1365596bedd7b
GarimaSChoudhary <choudhary.g@northeastern.edu>
2022-04-05 21:51:02 -0400

    assignment 8

--- src/main/java/com/cloud/application/controller/Assignment1Controller.java
+++ src/main/java/com/cloud/application/controller/Assignment1Controller.java
@@ -1,15 +1,28 @@
 package com.cloud.application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.timgroup.statsd.StatsDClient;
+
 @RestController
 public class Assignment1Controller{
+
+	@Autowired
+    private StatsDClient statsd;
+	
 	@GetMapping(value="/healthz")
 	public ResponseEntity<String> getMethodName() {
+       
+		statsd.increment("Calls - Get Healthz");
+        long start = System.currentTimeMillis();
+		statsd.recordExecutionTime("DB Response Time - Get user/self", System.currentTimeMillis() - start);
+
 		return new ResponseEntity<>("Hello", HttpStatus.OK);
+		
 
 	}
 }
