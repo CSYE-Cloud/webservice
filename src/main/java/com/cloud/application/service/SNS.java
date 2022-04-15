dab9adbd48385ccbce7e5a00148ca13ce0c527c6
GarimaSChoudhary <choudhary.g@northeastern.edu>
2022-04-14 23:21:03 -0400

    logs

--- src/main/java/com/cloud/application/service/SNS.java
+++ src/main/java/com/cloud/application/service/SNS.java
@@ -53,16 +53,16 @@ public class SNS {
             System.out.println("Message " + publishResponse.messageId() + "is successfully published to SNS Topic 'SNSTopic'");
 
 
-            AmazonDynamoDB awsClient = AmazonDynamoDBClientBuilder.standard().build();
-            DynamoDB dynamo = new DynamoDB(awsClient);
-            Table table = dynamo.getTable("AccountDetails");
-            long now = Instant.now().getEpochSecond(); // unix time
-            long ttl = 120; // 2 mins for demo
-            Item item = new Item()
-                    .withPrimaryKey("emailID", rEmail)
-                    .with("token",randomToken)
-                    .with("TimeToLive",ttl + now);
-            PutItemOutcome outcome = table.putItem(item);
+//            AmazonDynamoDB awsClient = AmazonDynamoDBClientBuilder.standard().build();
+//            DynamoDB dynamo = new DynamoDB(awsClient);
+//            Table table = dynamo.getTable("AccountDetails");
+//            long now = Instant.now().getEpochSecond(); // unix time
+//            long ttl = 120; // 2 mins for demo
+//            Item item = new Item()
+//                    .withPrimaryKey("emailID", rEmail)
+//                    .with("token",randomToken)
+//                    .with("TimeToLive",ttl + now);
+//            PutItemOutcome outcome = table.putItem(item);
 //            
             //logger.info("Message " + result.messageId() + " is successfully published to SNS Topic 'Notification_Email'.");
         } catch (SnsException e) {
